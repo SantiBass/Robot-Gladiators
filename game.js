815593d8812beab21c14b12fe996771101d0c652
SantiBass <sibarraryan@gmail.com>
2021-10-14 15:40:46 -0700

    using funtions

---
+++ game.js
@@ -0,0 +1,13 @@
+window.alert("This is an alert! JavaScript is running!");
+
+
+// this creates a fuctionnamed "fight"//
+function fight(){
+    window.alert("The fight has begun");
+
+}
+
+
+
+
+fight();
